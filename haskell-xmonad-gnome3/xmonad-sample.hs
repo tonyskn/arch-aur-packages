8c8d22468b6cb8ca8348f46d8bcc0356f4bb00a9
tonyskn <tonyskn@gmail.com>
2014-10-22 07:58:27 +0200

    [xmonad-gnome3] Add separate version pulling dependencies from ArchHaskell

---
+++ haskell-xmonad-gnome3/xmonad-sample.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -fno-warn-missing-signatures #-}
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig
+
+myConfig = defaultConfig { modMask = mod4Mask, terminal = "xfce4-terminal" }
+    `additionalKeysP`
+        [ ("M-f", spawn "nautilus --no-desktop ~/Downloads")
+        , ("M-S-q", spawn "sudo systemctl stop xlogin@USER")
+        , ("M-S-,", spawn "gnome-control-center") ]
+
+main = xmonad =<< xmobar myConfig
